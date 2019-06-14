ebcfe3377f404ee1ca945106e4169846add3cc88
Brandi Clinard <brandi@Brandis-MacBook-Pro.local>
2019-06-14 12:12:31 -0500

    updates/ completed exercises

---
+++ js/conditionals.js
@@ -0,0 +1,228 @@
+(function(){
+"use strict";
+
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * if what the user enters is not a number, use an alert to tell them that, and
+ * do *not* display any of the above information.
+ *
+ * Can you refactor your code to use functions?
+ */
+
+// var play = confirm("Would you like to enter a number?");
+//
+// if (play) {
+//     var numPicked = Number(prompt('Please type a number.'));
+//     if (isNaN(numPicked)){
+//         alert("You did not enter a number.")
+//     } else {
+//         var evenOdd = (numPicked % 2 === 0) ? (numPicked + " is even.") : (numPicked + " is odd.");
+//         alert(evenOdd);
+//         var bigNum = numPicked + 100;
+//         alert('Your number plus 100 is ' + bigNum + ".");
+//         var negPos = (Math.sign(numPicked) === 1) ? (numPicked + " is positive.") : (numPicked + " is negative.");
+//         alert(negPos);
+//     }
+// } else {
+//     alert("Please let us know if you would like to play later.");
+// }
+
+
+
+
+/* ########################################################################## */
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color everytime the page loads)
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message that related to that
+ * color. Only worry about the colors defined above, if the color passed is not
+ * one of the ones defined above, return a message that says so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+
+// function analyzeColor(a){
+//     if ('red'){
+//         alert('Apples are red.');
+//         console.log('Apples are red.');
+//     } else if ('orange') {
+//         alert('Obviously the fruit oranges are orange.');
+//         console.log('Obviously the fruit oranges are orange.');
+//     } else if ('yellow') {
+//         alert("Don't eat the yellow snow.");
+//         console.log("Don't eat the yellow snow.");
+//     } else if ('green') {
+//         alert("The grass and trees are usually green when healthy.");
+//         console.log("The grass and trees are usually green when healthy.");
+//     } else if ('blue') {
+//         alert("The ocean is blue.");
+//         console.log("The ocean is blue.");
+//     } else if ('indigo') {
+//         alert("Native Americans used to make indigo paint for use on carcass canvas.");
+//         console.log("Native Americans used to make indigo paint for use on carcass canvas.");
+//     } else if ('violet') {
+//         alert("Isn't violet the same color as purple?");
+//         console.log("Isn't violet the same color as purple?");
+//     } else {
+//         alert("The color " + a + " is not listed on our analyzer at this time.");
+//         console.log("The color " + a + " is not listed on our analyzer at this time.");
+//     }
+// }
+//
+// console.log(analyzeColor(a));
+
+
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your function and console.log the results.
+ * You should see a different message everytime you refresh the page
+ */
+
+// console.log(analyzeColor(randomColor));
+
+/**
+ * TODO:
+ * Refactor your above function to use a switch-case statement
+ */
+
+// function analyzeColor(a){
+//     switch(a) {
+//         case 'red':
+//             alert('Apples are red.');
+//             console.log('Apples are red.');
+//             break;
+//         case 'orange':
+//             alert('Obviously the fruit oranges are orange.');
+//             console.log('Obviously the fruit oranges are orange.');
+//             break;
+//         case 'yellow':
+//             alert("Don't eat the yellow snow.");
+//             console.log("Don't eat the yellow snow.");
+//             break;
+//         case 'green':
+//             alert("The grass and trees are usually green when healthy.");
+//             console.log("The grass and trees are usually green when healthy.");
+//             break;
+//         case 'blue':
+//             alert("The ocean is blue.");
+//             console.log("The ocean is blue.");
+//             break;
+//         case 'indigo':
+//             alert("Native Americans used to make indigo paint for use on carcass canvas.");
+//             console.log("Native Americans used to make indigo paint for use on carcass canvas.");
+//             break;
+//         case 'violet':
+//             alert("Isn't violet the same color as purple?");
+//             console.log("Isn't violet the same color as purple?");
+//             break;
+//         default:
+//             alert("The color " + a + " is not listed on our analyzer at this time.");
+//             console.log("The color " + a + " is not listed on our analyzer at this time.");
+//             break;
+//     }
+// }
+//
+// console.log(analyzeColor(randomColor));
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+
+// var color = prompt('Please enter a color.').toLowerCase();
+
+
+/* ########################################################################## */
+
+
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * all for free!.
+ *
+ * Write a function named `calculateTotal` that accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+
+// function calculateTotal(luckyNumber, totalBill){
+//     if (luckyNumber === 0){
+//         var noDiscount = (totalBill * 0);
+//         return noDiscount;
+//     } else if (luckyNumber === 1) {
+//         var discount10 = (totalBill - (totalBill * 0.10));
+//         return discount10;
+//     } else if (luckyNumber === 2) {
+//         var discount25 = (totalBill - (totalBill * 0.25));
+//         return discount25;
+//     } else if (luckyNumber === 3) {
+//         var discount35 = (totalBill - (totalBill * 0.35));
+//         return discount35;
+//     } else if (luckyNumber === 4) {
+//         var discount50 = (totalBill - (totalBill * 0.50));
+//         return discount50;
+//     } else if (luckyNumber === 5) {
+//         var discountFree = (totalBill - totalBill);
+//         return discountFree;
+//     }
+// }
+
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 6.
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+
+// Generate a random number between 0 and 6
+
+
+    // var luckyNumber = Math.floor(Math.random() * 6);
+    //
+    // var totalBill = Number(prompt("What was your total bill?"));
+    //
+    // var newBill = calculateTotal(luckyNumber, totalBill);
+    //
+    // alert("Your lucky number was " + luckyNumber + "! Lets see if you get a discount. Your total bill before hand was $" + totalBill + " and now your bill is $" + newBill + "!!!");
+
+}());
--- js/javascript_notes.js
+++ js/javascript_notes.js
@@ -81,8 +81,169 @@ function hidePassword(pass){
 console.log(hidePassword(password));
 
 
+/* conditional notes *******************************************************************************************************/
+
+//    to use conditionals in functions: I want to build a function that returns a string when I pass a number as an arguement.
+
+    function isItANumber(parameter) {
+        if (isNaN(parameter) === false) {
+            return "That's a number!"
+        }
+    }
+
+    console.log(isItANumber(77)); // That's a number!
+    console.log(isItANumber(BOB)); // undefined
+
+    // if else statments
+
+    //if i owned one, id get milk from my cow when i ran out. but since i dont, ill go to the store instead.
+
+    var haveCow = false;
+
+    if (haveCow) {
+        console.log("Milking Bessie.")
+    } else {
+        console.log("Going to Kroger.")
+    }
+
+    //boolean if else (unless otherwise defined by changing the value with boolean operator; this is best practice
+
+    // if (){
+    //     true
+    // } else {
+    //     false
+    // }
 
 
+//expand previous isItANumber fn.  i want to also return string when we arent passed a number. how do i refactor our function?
+
+    function isItANumber(parameter) {
+        if (isNaN(parameter) === false) {
+            return "That's a number!"
+        } else {
+            return "That is not a number..."
+        }
+    }
+
+
+    // if/else if/else = can continue on until all conditions have been described.
+
+    //if i owned a cow, id get milk from it when i ran out. but since i dont, i could go to the store, but luckily, i own a goat, so i can get milk from it instead.
+
+
+    var haveCow = false;
+    var haveGoat = true;
+
+    if (haveCow === true) { // be explicit to ensure there is no confusion
+        console.log("milking betty");
+    } else if (haveGoat === true) { // again with explicit
+        console.log("milking billy sue");
+    } else {
+        console.log("going to albertson's")
+    }
+
+    //make a fn that takes in number and will return three different strings for an arguement lesser than, greater than, or equal to 35
+
+    function isThisThirtyFive(parameter){
+        if (parameter > 35) {
+            return "the number" + parameter + " is greater than 35.";
+        } else if  (parameter < 35) {
+            return "the number" + parameter + " is greater than 35.";
+        } else if (parameter === 35) {
+            return "that is 35!";
+        } else {
+            return "that is not a number.";
+        }
+    }
+
+    console.log(isThisThirtyFive(34));
+    console.log(isThisThirtyFive(35));
+    console.log(isThisThirtyFive(36));
+    console.log(isThisThirtyFive('tim'));// on returns due to specificty of ===
+
+    // Ternary operator: value based on boolean evaluation
+
+    //writing ternary expressions:
+
+    // var message = (booleanValue) ? "operation was true." : "operation was false.';
+
+    // only two options available-- keep note of that-- either true or false-- left side is true and right side is false of :
+
+    //are we out of milk? if we are: ill head to the store, but if we do : i want to make a bowl of cereal.
+
+    var doWeHaveMilk = false; // can change to true to alter the outcome
+
+    var action = (doWeHaveMilk) ? "Let's make some Honey Nut Cheerios!" : "Going to Giant Eagle.";
+
+    console.log(action);
+
+    // change if/else version of isItANumber function. refractor as a ternary operator.
+
+    // function isItANumber(parameter) {
+    //     if (isNaN(parameter) === false) {
+    //         return "That's a number!"
+    //     } else {
+    //         return "That is not a number..."
+    //     }
+    // }
+
+        return (isNan(parameter)) ? "That's not a number..." : "that's a number!";
+    //left side is true due to not having a number be what the true side evaluates out to : right side is false due to having a number evaluation
+
+    console.log(isItANumber(17));
+
+    //SWITCH STATEMENTS -- statement with multiple conditions that will trigger based on value of variable; best practice to use if the variable will have many
+    // outcomes for different reasons--- can possibly alter to contains the string to output several cases.
+
+    var color = prompt('What is your fav color?').toLowerCase();
+
+    switch(color) {
+        case 'green':
+            alert('what a coincidence, that is my fav!');
+            break;
+        case 'orange':
+            alert('thats my brothers fav color');
+            break;
+        default:
+            alert(color + " is a weird color dude");
+            break;
+    }
+
+
+    //i dont have a cow, goat or camel.  i guess ill have to go to the store for milk
+
+    var animal = "n/a";
+
+    switch (animal) {
+        case "cow":
+            console.log("Milking Betty Lynn");
+            break;
+        case "goat":
+            console.log('Milking bertha');
+            break;
+        case 'camel':
+            console.log("milking ophelia");
+            break; // if you do not place the break-- you will fall through to the next case that matches input
+        default:
+            console.log("go to food lion");
+            break;
+    }
+
+    //user to input their fav sandwich meat. like olive loaf we return alert congratulate them on their bold choice, and if they choose brisket we should return different alert: this one for congratulating them on the 'right' answer. all other responses should trigger an alert stating apathy.
+
+    var lunchMeat = prompt("what is your fav sandwich meat?");
+
+    switch(lunchMeat) {
+        case "oliveloat":
+            alert("Congrats on your bold choice!");
+            break;
+        case "brisket":
+            alert("Thank you for choosing the right answer.");
+            break;
+        default:
+            alert("Meh..." + lunchMeat + " aint my speed, but you do you.");
+            break; //do not have to place default break but it is good practice
+    }
 
 //    this is the end of the IFFE below
 });
