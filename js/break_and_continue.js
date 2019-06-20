83b1a0cfdd27df65ebddfcc2c4daf8d861ddaeb3
Brandi Clinard <brandi@Brandis-MacBook-Pro.local>
2019-06-20 14:40:45 -0500

    EXERCISE COMPLETE WITH WALKTHROUGH NOTES ADDED

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,19 +4,21 @@
     /*****************Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.*
      */
 
+    // MY CODE
 
-// do {
-//     var numPick = prompt('Pick an odd number between 1 and 50.');
-//         if (numPick % 2 === 0){
-//             alert('That is not odd, please try again.')
-//         }else{
-//             alert('Congrats!')
-//             break;
-//         }
-//
-// } while (numPick <= 50);
+    // for (var i = 1; i < 50; i +=2){
+    //     var numPick = Number(prompt('Pick an odd number between 1 and 50.'));
+    //     if ((isNaN(numPick)) || (numPick < 1) || (numPick > 50)) {
+    //         alert('You did not pick an odd number within the stipulations. Try again.');
+    //     }else if(numPick % 2 === 0) {
+    //         alert('That is not odd, please try again.')
+    //
+    //     }else{
+    //         alert('Thank you.');
+    //         break;
+    //     }
+    // }
 
-//need to add caps for min and max number of 1-50; add else if for stipulations and another alert. need to add stipulations for string as well.
 
 /************  Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
  Your output should look like this:
@@ -51,33 +53,26 @@
  Here is an odd number: 49
  ****/
 
-var numPick = Number(prompt('Pick an odd number between 1 and 50.'));
-    var n = 0;
-            var oddNum;
-//     do {
-//
-//         if ((n % 2 !== 0) && ((numPick < 50) || (numPick > 1))) {
-//             n++;
-//             console.log("Here is an odd number: " + n);
-//             continue;
-//         }else if (oddNum === numPick){
-//             console.log('Skipping this number: ' + numPick);
-//         }else{
-//             alert('You did not enter an odd number between 1 and 50.');
-//             Number(prompt('Pick an odd number between 1 and 50.'));
-//         }
-//
-// } while (n < 50);
+// MY CODE
 
-for (n = 0; n < 50; n++){
-    if ((n % 2 !== 0)){
-        console.log("Here is an odd number:" + n);
-    // }else{
-    //     alert('You did not enter an odd number between 1 and 50.');
-    }
-    for (numPick; n < 50;n++){ numPick === n;
-        console.log("Skipping this number: " + numPick);
-    }
-}
+//     var numPick = Number(prompt('Pick an odd number between 1 and 50.'));
+//
+// while ((isNaN(numPick)) || (numPick < 1) || (numPick > 50) || (numPick % 2 === 0)) {
+//     alert('You did not pick an odd number within the stipulations. Try again.');
+//         if (true){
+//             numPick = Number(prompt('Pick an odd number between 1 and 50.'))
+//         } continue;
+// }
+//         for (var i = 1; i < 50; i += 2) {
+//             if (i === numPick) {
+//                 console.log("Skip this number: " + i);
+//             } else {
+//                 console.log('Here is an odd number: ' + i);
+//
+//             }
+//         }
+
 
 }());
+
+//--- EXERCISE COMPLETE. REVIEWED WALKTHROUGH NOTES ON CODEUP BETELGEUSE... DOES NOT STATE CONTINUE IN THIS FUNCTION. SET UP THE SAME WAY.
