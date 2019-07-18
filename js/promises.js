18201699012f55281b01293d4de61a5445f5d772
Brandi <brandiclinard021911@gmail.com>
2019-07-18 15:48:15 -0500

    EXERCISE COMPLETE WITH WALKTHROUGH NOTES

--- js/promises.js
+++ js/promises.js
@@ -2,44 +2,65 @@
 
 //Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 
+const wait = (num) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`This promise resolved after ${num} second(s)!`)
+        }, (num * 1000));
+    });
+};
+
 // const wait = (num) => {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             console.log(`This promise resolved after ${num} second(s)!`)
-//         }, (num * 1000));
-//     });
-// };
-//
-// wait(1);
-// wait(3);
-// wait(5);
-// wait(7);
-// wait(9);
+//     return new Promise((resolve) =>{
+//         setTimeout(resolve, ms);
+//     })
+// }
+
+wait(1);
+wait(3);
+wait(5);
+wait(7);
+wait(9);
 
 
 //Create a function that accepts a GitHub username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
 
-// const lastCommit ='https://api.github.com/repos/:owner/:repo/git/refs/heads/:branch';
 
-function lastCommit (userName) {
-    fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': 'token ' + githubUserEmailToken}})
+// function lastCommit (userName) {
+//     fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': 'token ' + githubToken}})
+//         .then(response => response.json())
+//         .then(data => { console.log(data[0].created_at)// attempt at looping through returned information
+//             // for(const[index, datum]of data.entries()){
+//             //     if(data[i].type === 'PushEvent'){
+//             //         console.log(this.created_at)
+//             //     }
+//             // }
+//         })
+// }
+//
+// lastCommit('brandiclinard');
+
+    //WALK THROUGH NOTES:
+const getLastPush = function (username) {
+    const url = `https://api.github.com/users/${username}/events`;
+    return fetch(url, {
+        headers: {
+            "Authorization": 'token ' + githubToken
+        }
+    })
         .then(response => response.json())
-        .then(dataArray => {
-            for (let user of dataArray){
-                if (data[i].type === 'PushEvent') {
-                    console.log(this.created_at)
+        .then(events => {
+            for(let event of events){
+                if (event.type == 'PushEvent'){
+                    return event.created_at
                 }
             }
         });
 }
 
-lastCommit('brandiclinard');
+console.log(getLastPush('brandiclinard'));
 
-    // .then(usersArray => {// HAVE TO USE SECOND .THEN IN ORDER TO ACCOMPLISH AFTER THE RESPONSE WITH JSON HAS RETURNED
-    //     for(let user of usersArray){
-    //         console.log(user.login, user.html_url)
-    //     }
-
-    // console.log(data)
+let username = prompt("what user do you want to check up on?");
+console.log(getLastPush(username));
 
 
